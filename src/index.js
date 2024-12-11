d0d4172d1912ee067df6dd25f5bf21816d889479
EricaAkos <ericadeku4@gmail.com>
2024-12-11 21:53:15 +0000

    added all files

---
+++ src/index.js
@@ -0,0 +1,53 @@
+
+// let heading = document.getElementById("heading");
+// heading.style.backgroundColor = "blue";
+// heading.style.color = "white";
+// heading.style.textAlign = "center";
+// heading.style.fontStyle = "italic";
+
+document.getElementById("quest1").addEventListener("click", function() {
+  const quest1 = document.getElementById('quest1');
+    quest1.innerHTML = 'I know now what the DOM is 🙌';
+    quest1.style.backgroundColor = "grey";
+    quest1.style.color = "black";
+});
+
+document.getElementById("quest2").addEventListener("click", function() {
+    const quest2 = document.getElementById("quest2");
+    quest2.innerHTML = "I know how to access/select elements in the DOM 👊";
+    quest2.style.backgroundColor = "yellow";
+    quest2.style.color = "black"
+});
+
+document.getElementById("quest3").addEventListener("click", function() {
+    const quest3 = document.getElementById("quest3");
+    quest3.innerHTML = "I know how to use the style methods to change the style of an element in the DOM ✅";
+    quest3.style.backgroundColor = "green";
+    quest3.style.color = "black"
+});
+
+document.getElementById("quest4").addEventListener("click", function() {
+    const quest4 = document.getElementById('quest4');
+    quest4.innerHTML = "I know how the styling properties differ in JS from CSS 👍";
+    quest4.style.backgroundColor = "purple";
+    quest4.style.color = "black"
+});
+
+document.getElementById("quest5").addEventListener("click", function() {
+    const quest5 = document.getElementById('quest5');
+    quest5.innerHTML = "Yes I did it!! I manipulated the DOM and styled my elements 🤩🤩🤩🤩";
+    quest5.style.backgroundColor = "black";
+    quest5.style.color = "white"
+});
+
+const viewImageButton = document.getElementById('open'); 
+const closeImageButton = document.getElementById('close'); 
+const imageContainer = document.getElementById('image');
+
+viewImageButton.addEventListener('click', function() { 
+    imageContainer.classList.remove('hidden'); 
+});
+
+closeImageButton.addEventListener('click', function() { 
+    imageContainer.classList.add('hidden'); 
+});
